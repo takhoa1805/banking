4f2c76f0a92ec11a282b19150f5f1f7901e1306e
vxmtrung <vuxuanmaitrung1821@gmail.com>
2024-11-06 11:23:40 +0700

    add logging statement typeorm

--- nestjs-app/ormconfig.ts
+++ nestjs-app/ormconfig.ts
@@ -1,23 +1,25 @@
 import { DataSource, type DataSourceOptions } from 'typeorm';
 import { type SeederOptions } from 'typeorm-extension';
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 require('dotenv').config({ path: '../.env' });
 
 const option: DataSourceOptions & SeederOptions = {
-    type: 'mysql',
-    host: process.env.MYSQLDB_HOST,
-    port: Number(process.env.MYSQLDB_LOCAL_PORT),
-    username: process.env.MYSQLDB_USER,
-    password: process.env.MYSQLDB_PASSWORD,
-    database: process.env.MYSQLDB_DATABASE,
-    synchronize: false,
-    entities: [
-        __dirname + '/src/**/*.entity.{ts,js}',
-        __dirname + 'src/modules/**/**/entities/*.entity{.ts,.js}',
-        __dirname + 'src/modules/**/*.view-entity{.ts,.js}',
-      ], 
-    migrations: ['src/migrations/*{.ts,.js}'],
-    migrationsRun: false,
-}
+  type: 'mysql',
+  host: process.env.MYSQLDB_HOST,
+  port: Number(process.env.MYSQLDB_LOCAL_PORT),
+  username: process.env.MYSQLDB_USER,
+  password: process.env.MYSQLDB_PASSWORD,
+  database: process.env.MYSQLDB_DATABASE,
+  synchronize: false,
+  entities: [
+    __dirname + '/src/**/*.entity.{ts,js}',
+    __dirname + 'src/modules/**/**/entities/*.entity{.ts,.js}',
+    __dirname + 'src/modules/**/*.view-entity{.ts,.js}',
+  ],
+  migrations: ['src/migrations/*{.ts,.js}'],
+  migrationsRun: false,
+  logging: true,
+};
 
 export const dataSource = new DataSource(option);
